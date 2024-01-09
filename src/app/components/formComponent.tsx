78ec71dfc6c777679df2536da8cd11e013f1147e
divjot-bst <divjot.singh@bluestacks.com>
2024-01-09 13:43:35 +0530

    start deployment

--- src/app/components/formComponent.tsx
+++ src/app/components/formComponent.tsx
@@ -75,7 +75,7 @@ export default function FormComponent() {
     return (
         <>
         <form onSubmit={handleButtonClick}>
-            <p>TOKLAS, Surrey St, Temple, London WC2R 2ND, 7PM 24.02.2024</p>
+            <p>TOKLAS, Surrey St, Temple, London WC2R 2ND, 7PM  24.02.2024</p>
             <div className="row input-row">
                 <input name="first-name" value={fName} type="text" id="first-name" placeholder="First name" onChange={onInputChange} required />
                 <input name="last-name" value={lName} type="text" id="last-name" placeholder="Last name" onChange={onInputChange} required />
